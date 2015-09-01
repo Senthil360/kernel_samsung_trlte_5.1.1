02e3a142c629888ec4012edd48d6558e59a98b33
friedrich420 <kelpidorou@yahoo.com>
2015-09-01 12:00:28 +0300

    cpufreq: force cpuN policy to match cpu0 [imoseyon & twistedumbrella]
    Conflicts:
    	drivers/cpufreq/cpufreq.c

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1859,6 +1859,7 @@ int cpufreq_driver_target(struct cpufreq_policy *policy,
 }
 EXPORT_SYMBOL_GPL(cpufreq_driver_target);
 
+
 int __cpufreq_driver_getavg(struct cpufreq_policy *policy, unsigned int cpu)
 {
     int ret = 0;
@@ -2048,6 +2049,7 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 				struct cpufreq_policy *new_policy)
 {
 	int ret = 0, failed = 1;
+	struct cpufreq_policy *cpu0_policy = NULL;
 
 	pr_debug("setting new policy for CPU %u: %u - %u kHz\n", new_policy->cpu,
 		new_policy->min, new_policy->max);
@@ -2085,8 +2087,14 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_NOTIFY, new_policy);
 
-	policy->min = new_policy->min;
-	policy->max = new_policy->max;
+	if (policy->cpu >= 1) {
+		cpu0_policy = cpufreq_cpu_get(0);
+		policy->min = cpu0_policy->min;
+		policy->max = cpu0_policy->max;
+	} else {
+        policy->min = new_policy->min;
+        policy->max = new_policy->max;
+	}
 
 	pr_debug("new min and max freqs are %u - %u kHz\n",
 					policy->min, policy->max);
@@ -2112,7 +2120,12 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 			}
 
 			/* start new governor */
-			policy->governor = new_policy->governor;
+			
+		if (policy->cpu >= 1 && cpu0_policy) {
+                    policy->governor = cpu0_policy->governor;
+ 	        } else {
+                    policy->governor = new_policy->governor;
+                }
 			if (!__cpufreq_governor(policy, CPUFREQ_GOV_POLICY_INIT)) {
 				if (!__cpufreq_governor(policy, CPUFREQ_GOV_START)) {
 					failed = 0;
