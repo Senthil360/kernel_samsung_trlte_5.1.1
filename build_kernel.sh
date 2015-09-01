0a13a3fc44d55b3d9bb4919a6a374424a100cc0a
crimsonthunder <crimsonthunder54@gmail.com>
2015-09-01 14:05:18 +0200

    live is easy
    Conflicts:
    	build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+export ARCH=arm
+export CROSS_COMPILE=/home/florian/arm-eabi-4.9/bin/arm-eabi-
+mkdir output
 
-mkdir $(pwd)/out
-make -C $(pwd) O=$(pwd)/out VARIANT_DEFCONFIG=apq8084_sec_trlte_eur_defconfig apq8084_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig TIMA_DEFCONFIG=tima_defconfig DMVERITY_DEFCONFIG=dmverity_defconfig
-make -C $(pwd) O=$(pwd)/out
-cp $(pwd)/out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+make -C $(pwd) O=$(pwd)/output VARIANT_DEFCONFIG=apq8084_sec_trlte_eur_defconfig apq8084_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig TIMA_DEFCONFIG=tima_defconfig DMVERITY_DEFCONFIG=dmverity_defconfig
+make -C $(pwd) O=output -j12
+
+mv output/arch/arm/boot/zImage $(pwd)/AIK-Linux/split_img/boot.img-zImage
+cd AIK-Linux/
+bash repackimg.sh
+cd .. 
+mv AIK-Linux/image-new.img $(pwd)/Kernel.img
+rm  -r output/
