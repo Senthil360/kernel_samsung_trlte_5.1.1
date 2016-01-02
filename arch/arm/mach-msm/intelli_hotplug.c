dc09ccb95afdf2a28c2b0f895b547a5dd1b3db33
crimsonthunder54 <crimsonthunder54@gmail.com>
2016-01-02 12:18:56 +0100

    fix compile

--- arch/arm/mach-msm/intelli_hotplug.c
+++ arch/arm/mach-msm/intelli_hotplug.c
@@ -336,16 +336,6 @@ static void __ref intelli_plug_resume(struct work_struct *work)
 		}
 	}
 
-	if (wakeup_boost || required_wakeup) {
-		/* Fire up all CPUs */
-		for_each_cpu_not(cpu, cpu_online_mask) {
-			if (cpu == 0)
-				continue;
-			cpu_up(cpu);
-			apply_down_lock(cpu);
-		}
-	}
-
 	/* Resume hotplug workqueue if required */
 	if (required_reschedule)
 		queue_delayed_work_on(0, intelliplug_wq, &intelli_plug_work,
