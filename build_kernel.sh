6d8a7e3b2beb7e876e243f50d90743e234be84bc
crimsonthunder54 <crimsonthunder54@gmail.com>
2016-01-03 22:18:05 +0100

    new imgtools

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,9 +5,8 @@ mkdir output
 make -C $(pwd) O=$(pwd)/output VARIANT_DEFCONFIG=apq8084_sec_trlte_eur_defconfig apq8084_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig TIMA_DEFCONFIG=tima_defconfig DMVERITY_DEFCONFIG=dmverity_defconfig
 make -C $(pwd) O=output -j12
 
-mv output/arch/arm/boot/zImage $(pwd)/AIK-Linux/split_img/boot.img-zImage
-cd AIK-Linux/
-bash repackimg.sh
-cd .. 
-mv AIK-Linux/image-new.img $(pwd)/Kernel.img
-rm  -r output/
+mv output/arch/arm/boot/zImage $(pwd)/imgtools/Boot/kernel
+./imgtools/dtbTool -o ./imgtools/Boot/dt.img -s 4096 -p ./output/scripts/dtc/ ./output/arch/arm/boot/dts/
+bash ./imgtools/mkboot ./imgtools/Boot ./imgtools/Boot.img
+mv ./imgtools/Boot.img ./Boot.img
+rm ./output
